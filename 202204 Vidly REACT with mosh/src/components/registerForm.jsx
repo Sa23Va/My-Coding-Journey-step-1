7ad7489f1581a9b69fe69c4188fb48ba29a6c702
Sandra <100031837+Sa23Va@users.noreply.github.com>
2022-04-30 14:12:47 +0300

    Add files via upload

---
+++ 202204 Vidly REACT with mosh/src/components/registerForm.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Joi from 'joi-browser';
+import Form from './common/form';
+
+
+class RegisterForm extends Form {
+    state = { 
+        data:{username: '', password: '', name: ''},
+        errors: {}
+     };
+
+    schema = {
+        username: Joi.string().required().label('Username'),
+        password: Joi.string().required().min(5).label('Password'),
+        name: Joi.string().required().label('Name'),
+
+    }
+
+    doSubmit = () => {
+        console.log('Submitted');
+    };
+
+    render() { 
+        return (
+            <div>
+                <h2>Register</h2>
+                <form onSubmit={this.handleSubmit}>
+                    {this.renderInput('username', 'Username')}
+                    {this.renderInput('password', 'Password', 'password')}
+                    {this.renderInput('name', 'Name' )}
+                    {this.renderButton('Register')}
+
+                </form>
+            </div>
+        );
+    }
+}
+ 
+export default RegisterForm;
