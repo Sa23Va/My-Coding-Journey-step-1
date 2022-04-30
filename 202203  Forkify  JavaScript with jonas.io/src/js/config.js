40e99a0232bf2e144c7a633ac4a14368ff7b0d8d
Sandra <100031837+Sa23Va@users.noreply.github.com>
2022-04-30 14:05:02 +0300

    Add files via upload

---
+++ 202203  Forkify  JavaScript with jonas.io/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '84f882da-c230-4aa4-b841-ecb8f545a04e';
+export const MODAL_CLOSE_SEC = 2.5;
